816c6539915ff9c4c39ed443746e7588fca4f588
kashyapnathan <43067072+kashyapnathan@users.noreply.github.com>
2018-09-27 08:08:07 -0400

    Add files via upload

---
+++ src/VariableTest.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ * @author knathan22
+ *
+ */
+public class VariableTest {
+
+	public static void main(String[] args) 
+	{
+	char firstInitial;
+	String lastInitial;
+	int age;
+	
+	firstInitial= 'K';
+	lastInitial= "N";
+	age = 14;
+	
+	System.out.print("My first initial is " + firstInitial + ", my  last initial is " + lastInitial + ", and my age is " + age);
+
+	}
+
+}
