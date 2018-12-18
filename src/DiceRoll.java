82c3e34781d15c12b350d9a787afb10152385a3f
kashyapnathan <43067072+kashyapnathan@users.noreply.github.com>
2018-12-18 18:41:40 -0500

    Update DiceRoll.java

--- src/DiceRoll.java
+++ src/DiceRoll.java
@@ -1,9 +1,37 @@
+import java.util.Random;
+import java.util.Scanner;
 
-public class DiceRoll {
+/**
+ * Kashyap Nathan
+ */
+public class DiceRoll{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		Random dice = new Random();
+		Scanner getDisBread = new Scanner(System.in);
+		System.out.println("Please enter a number from 1 to 6. This number will be the target number for the dice");
+		int userInput = getDisBread.nextInt();
+		int diceNumber = dice.nextInt(6)+1;
+		int count = 0;
+		
+		
+			while(userInput<1 || userInput>6)
+			{
+				System.out.println("Invalid number, please enter a valid one.");
+				userInput=getDisBread.nextInt();
+				getDisBread.nextLine();
+			}
+		
+			while(userInput != diceNumber)
+			{
+			count +=1;
+			System.out.println("The dice has rolled a: "+diceNumber+". Press enter to roll once more.");
+			getDisBread.nextLine();
+			diceNumber=dice.nextInt(6)+1;
+			}
+			System.out.println("Congragulations, you have rolled a "+userInput +" after "+count+" attempts");
+	
+	
 
-	}
-
-}
+}}
